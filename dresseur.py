7168d6fdcc7ee7428b8ecba91d1f586ee83dae88
remi_mrd <remi.menard06@gmail.com>
2024-05-03 17:01:59 +0200

    dresseur_0305

---
+++ dresseur.py
@@ -0,0 +1,61 @@
+#from mael import Pokemon
+import numpy as np
+
+########## A SUPPRIMER #########
+class Pokemon:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+########## A SUPPRIMER #########
+
+class Dresseur:
+
+    SIZE = 10
+    DISTANCE_COMBAT = 3
+    
+    def __init__(self, x, y, lst_pokemons):
+        self.x = x
+        self.y = y
+        self.pokedex = lst_pokemons
+
+    def deplacement(self, dx, dy):
+        # Change les coordonnées du dresseur
+        self.x = self.x + dx
+        self.y = self.y + dy
+
+    def distance(self, pokemon):
+        # Renvoie la distance du dresseur à un pokemon
+        return np.sqrt((self.x - pokemon.x)**2 + (self.y - pokemon.y)**2)
+
+    def proche(self, pokemons_sauvages):
+        """
+            Renvoie un couple :
+            True si assez proche d'un pokemon sauvage, et l'indice dans la liste pokemons_sauvage correspondant au pokemon recontré
+        """
+        cpt_pk = 0
+        for pk in pokemons_sauvages:
+            if self.distance(pk) <= self.DISTANCE_COMBAT:
+                return (True, cpt_pk)
+            cpt_pk += 1
+        return (False, cpt_pk)
+    
+    def get_new_pokemon(self, pokemon):
+        """
+            Ajoute un pokemon au pokédex
+        """
+        copie_pokemon = pokemon.duplic() # méthode à écrire de copie de pokémon (pour qu'ils ne soient pas à la même adresse)
+        self.pokedex.append(copie_pokemon)
+    
+    
+
+    
+if __name__ == "__main__":
+    pk1 = Pokemon(2, 2)
+    pk2 = Pokemon(5, 5)
+    lst_pk = [pk2, pk1]
+    d = Dresseur(1, 1, [])
+    
+    pk_proche = d.proche(lst_pk)
+    if pk_proche[0]:
+        id_pk = pk_proche[1]
+        print(id_pk)
