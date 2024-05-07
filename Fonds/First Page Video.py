b279a867725f9c72a383197bef4ace50fa79e93e
diegoposba <diegoposba@gmail.com>
2024-05-07 16:46:23 +0200

    Ajout du visu pour Combat + clarification des fich

--- Fonds/First Page Video.py
+++ Fonds/First Page Video.py
@@ -1,43 +1,32 @@
 import sys
 import cv2
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer
-
+from FirstOnePage import Ui_MainWindow 
 
 ### IMPORTANT: IL FAUT INSTALLER OPENCV AVEC LA COMMANDE SUIVANTE: pip install opencv-python
 
 
-class VideoPlayer(QWidget):
+class VideoPlayer(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        
+        self.ui = Ui_MainWindow()  # Instancier la classe Ui_MainWindow
+        self.ui.setupUi(self)  # Configurer le MainWindow à l'aide de Ui_MainWindow
         self.loop_video = True  # Définir loop_video à True par défaut
-        
         self.init_ui()
-        
+
     def init_ui(self):
         self.setWindowTitle("Video Player")
-        
-        # Create a timer for video playback
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
-        
-        # Load video from file
         self.video_path = "Fonds/Pikachu Pixel Animated Loop.mp4"  # Replace with your video file path
         self.cap = cv2.VideoCapture(self.video_path)
-        
-        # Start video playback
         self.start_video()
-        
-        # Create a layout for the player
-        layout = QVBoxLayout(self)
-        self.label = QLabel(self)
-        layout.addWidget(self.label)
-        
+
     def start_video(self):
         self.timer.start(30)  # 30 ms per frame
-        
+
     def update_frame(self):
         ret, frame = self.cap.read()
         if ret:
@@ -46,12 +35,13 @@ class VideoPlayer(QWidget):
             bytes_per_line = ch * w
             q_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
             pixmap = QPixmap.fromImage(q_image)
-            self.label.setPixmap(pixmap)
+            self.ui.Pokemon2.setPixmap(pixmap)  # Afficher la vidéo sur l'étiquette label
+            self.ui.Pokemon2.resize(w, h)  # Redimensionner l'étiquette pour s'adapter à la taille de la vidéo
+
+            # Afficher l'image Pokémon_logo.png au-dessus de la vidéo
             
-            # Resize the window to match the size of the video
-            self.resize(w, h)
+
         else:
-            # Rewind video to the beginning if looping is enabled
             if self.loop_video:
                 self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
             else:
@@ -65,4 +55,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = VideoPlayer()
     window.show()
+    window.resize(app.desktop().size())  # Redimensionner la fenêtre pour couvrir tout l'écran
+    window.showNormal()
     sys.exit(app.exec_())
